667195c3f24e55c55efa2f4e792f419141e34e91
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-17 02:40:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,6 +50,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  number = initial_round
+  until number >= 21
+    hit?(number)
+  end
+  end_game(number)
 end
+  
     
